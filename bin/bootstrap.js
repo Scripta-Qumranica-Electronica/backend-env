32a2b873e81b85cdb8ea0d1161aaa1894df054f8
Itay Zandbank <itay@chelem.co.il>
2018-10-30 22:38:39 +0200

    Remove websockets

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -154,6 +154,7 @@ if (versions.dependencies["Data-files"]) {
 
 console.log(chalk.green('✓ Successfully checked out the proper Data-files branch/tag.'))
 
+/*
 console.log(chalk.blue('Installing websocket dependencies with yarn...'))
 cmd = spawnSync(yarn, ['--pure-lockfile'], { encoding : 'utf8', cwd: './resources/socket', stdio: [null, process.stdout, process.stderr] })
 if (cmd.status !== 0) {
@@ -161,7 +162,7 @@ if (cmd.status !== 0) {
     console.log('✗ You may be missing either node/npm or yarn.')
     process.exit(1)
 }
-
+*/
 console.log(chalk.green('✓ All necessary node dependencies have been installed.'))
 
 console.log(chalk.blue('Building the Docker containers with docker-compose.'))
